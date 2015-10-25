861828401e634d6f2c38b6647871ba8fbe6378a2
Oleg Dovger <oleg.a.dovger@gmail.com>
2015-10-25 22:33:14 +0300

    check tests

--- test/simple-test.js
+++ test/simple-test.js
@@ -1,7 +1,15 @@
 var assert = require('chai').assert;
+var Person = require('../src/person.es6');
 
 describe('Simple test', () => {
   it('numbers are equal', () => {
     assert.equal(42, 42);
+  });
+
+
+  it('Person name check', () => {
+    let user = new Person('Oleg Dovger');
+
+    assert(user.fullName, 'Oleg Dovger');
   });
 });
